fa967570f9ea6aae7a33fdbc5e1a8d466aeb305d
danipier768 <danielmauricioperezrojas@gmail.com>
2024-06-05 16:55:56 -0500

    commit

---
+++ src/components/BodyOneComponent.jsx
@@ -0,0 +1,14 @@
+const BodyCompont = () => {
+    return(
+        <section className="container">
+            <div className="container-sm mw-75">
+                <div className="row">
+                    <div className="col-4">d</div>
+                    <div className="col-4">d</div>
+                    <div className="col-4">s</div>
+                </div>
+            </div>
+        </section>
+    )
+} 
+export default BodyCompont
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,41 @@
+    const HeaderCompont = () =>{
+        return(
+        <section className="container-fluid">
+            <div className="container-sm mw-75">
+                <div className="row m">
+                    <div className="row m-0 ">
+                        <div className="col-12 p-0 conteiner-image"> 
+                            <div className="row pt-5">
+                                <div className="col-1 text-white pag-of px-0 d-flex ">
+                                    <img src="https://cdn2.thedogapi.com/images/IBkYVm4v1.jpg" alt="" className="w-25"/>
+                                    <strong>PaginaOficial</strong>
+                                </div>
+                            </div>  
+                        </div>
+                        
+                    </div>
+                    <div className="row pt-5">
+                        <div className="col-12 text-white">
+                            <h1>Quieres programar?</h1>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 text-white my-2">
+                            <p>Dale click al boton y aprende a programar, es muy facil y sencillo, no te pierdas.</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6 pb-4">
+                            <button type="button" className="btn btn-danger w-25"><strong>Ingresa aqui</strong></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+        </section>
+        
+        
+        )
+    
+}
+ export default HeaderCompont
